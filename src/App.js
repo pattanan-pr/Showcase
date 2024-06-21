74d006fd7ac423176a9a1e4f1e2450a5cfe4694e
Pattanan <pattanan@Ampere-2.local>
2024-06-21 12:19:54 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+// import {
+//   EffectComposer,
+//   DepthOfField,
+//   Bloom,
+//   ChromaticAberration,
+// } from "@react-three/postprocessing";
+// import { BlendFunction } from "postprocessing";
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import "./style.css";
+
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls 
+        target={[0, 0.35, 0]}
+        maxPolarAngle={1.45}
+      />
+
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+     <mesh>
+      <boxGeometry args={[1,1,1]}/>
+      <meshBasicMaterial color={'red'} />
+    </mesh>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
   );
 }
 
